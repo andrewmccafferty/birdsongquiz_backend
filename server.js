389801263a2521ce2a0f4666daf2b13e60daec29
Andrew McCafferty <andrew.mccafferty@theappbusiness.com>
2018-04-21 17:31:19 +0100

    Switch to using Scientific name

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const getRandomArrayElement = (items) => {
 
 const selectRandomSpecies = () => {
     let randomElement = getRandomArrayElement(speciesList);
-    return randomElement.Species;
+    return randomElement.ScientificName;
 };
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
