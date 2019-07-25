c8a6d5184a33a9132dc5fe54859c9bc34617206a
Andrew McCafferty <andrewmc@dunnhumby.com>
2019-07-25 22:50:08 +0100

    add image url to API response

--- src/service/recordings.js
+++ src/service/recordings.js
@@ -1,31 +1,49 @@
 const requestPromise = require('request-promise');
 const arrayHelpers = require('../util/arrayHelpers');
 
-const getRandomRecording = (species) => {
+const getImageForSpecies = async (species) => {
+    try {
+        const url = `https://api.flickr.com/services/feeds/photos_public.gne?tags=${encodeURIComponent(species)}&format=json&nojsoncallback=1`
+        const response = await requestPromise(url)
+        const parsedResponse = JSON.parse(response)
+        const getImageFromResponse = () => {
+            const item = parsedResponse.items[0]
+            return {
+                imageUrl: item.media["m"],
+                flickrLink: item.link,
+                photographer: item.author
+            }
+        }
+        return getImageFromResponse()
+    } catch (e) {
+        console.error('Error retrieving image from Flickr', e)
+        return null
+    }
+}
+
+const getRandomRecording = async (species) => {
     let url = `https://www.xeno-canto.org/api/2/recordings?query=${encodeURIComponent(species)}`;
-    return requestPromise(url).then((body) => {
-        let parsedBody = JSON.parse(body);
-
-        let recordings = parsedBody.recordings;
-
-        if (!recordings || recordings.length == 0) {
-            return {
-                noRecordings: true
-            };
-        }
-
-        let randomRecording = arrayHelpers.getRandomArrayElement(recordings);
-        if (!randomRecording) {
-            return {
-                noRecordings: true
-            };
-        }
+    let body = await requestPromise(url);
+    const imageResult = await getImageForSpecies(species)
+    let parsedBody = JSON.parse(body);
+    let recordings = parsedBody.recordings;
+    if (!recordings || recordings.length == 0) {
         return {
-            recording: randomRecording
+            noRecordings: true
         };
-    });
+    }
+    let randomRecording = arrayHelpers.getRandomArrayElement(recordings);
+    if (!randomRecording) {
+        return {
+            noRecordings: true
+        };
+    }
+    return {
+        recording: randomRecording,
+        image: imageResult
+    };
 };
 
-module.exports =  {
+module.exports = {
     getRandomRecording: getRandomRecording
 }
