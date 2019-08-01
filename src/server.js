39a6016af28c82ecbb1a68561eac9ea2d911cf2a
Andrew McCafferty <andrew.mccafferty@theappbusiness.com>
2019-08-01 12:39:35 +0100

    added ip checking

--- src/server.js
+++ src/server.js
@@ -35,6 +35,7 @@ app.get('/api/birdsong', async (req, res) => {
 
 app.get('/api/test', (req, res) => {
     var ip = req.header('x-forwarded-for') || req.connection.remoteAddress
+    console.log(`ip captured was ${ip}`)
     res.send({yourIp: ip})
 });
 
